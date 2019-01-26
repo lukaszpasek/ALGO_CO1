295df93bf747fea3520231ab4f4f6206357c6bc1
LukaszPasek <paseczek2008@wp.pl>
2019-01-26 19:59:12 +0100

    Add files via upload

---
+++ C01.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+double x,y,c;
+int recursion(double mini , double maxi)
+{
+     double middle=(maxi+mini)/2;
+     double hx=sqrt(abs(pow(x,2)-pow(middle,2)));
+     double hy=sqrt(abs(pow(y,2)-pow(middle,2)));
+    if(maxi-mini<0.001) {
+            cout<<maxi<<endl;
+            return maxi;
+    }
+    if(c>hx*hy/(hy+hx)){
+            recursion(mini,middle);
+    }
+    if(c<hx*hy/(hy+hx)) {
+            recursion(middle,maxi);
+    }
+
+}
+int main()
+{
+   cout.precision(5);
+   ios_base::sync_with_stdio(false);
+    int t;
+    cin>>t;
+    for(int i=0 ; i<t ; i++)
+    {
+         double mini=0,maxi;
+        cin>>x>>y>>c;
+        maxi=min(x,y);
+
+        recursion(mini,maxi);
+
+    }
+    return 0;
+}
